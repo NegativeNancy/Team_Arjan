06a4f0579017d066f0cf00a5ba1e880e9b63171e
Negative Nancy <Negative Nancy@users.noreply.github.com>
2018-04-12 22:22:42 +0200

    Fix

--- demo/Connections.py
+++
@@ -1,8 +0,0 @@
-class Connections(object):
-
-    def __init__(self, begin, end, time):
-        self.begin = begin
-        self.end = end
-        self.time = time
-
-    
--- demo/README.md
+++
@@ -1,3 +0,0 @@
-# Team_Arjan
-
-To run this demo you will need to have a running flask setup.

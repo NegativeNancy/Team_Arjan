dcfae7041bc1262e1317de059404257eb9279592
emmahokken <emmahokken@wcw-stud-145-109-39-27.wireless.uva.nl>
2018-04-17 11:32:39 +0200

    so many changes

--- Route.py
+++ Route.py
@@ -1,5 +1,10 @@
 class Route():
+    """Class that holds single route objects."""
 
-    def __init__(self, route_list, time):
-        self.route_list = route_list
-        self.time = time
+    def __init__(self, connection_list):
+        """Initialise class."""
+        self.connection_list = connection_list
+
+        self.time = 0
+        for connection in self.connection_list:
+            self.time += connection.time
--- main.py
+++ main.py
@@ -1,34 +1,68 @@
 import Stations as st
 import Connections as cs
+import Route as rt
+import Solution as sn
+import random
+
+connections = list()
 def holland_main():
     load_connections()
     load_stations()
 
 def load_connections():
+    """Loads the connections into the designated object."""
     connection_list = []
     connections_file = open("files/ConnectiesHolland.csv")
+    # Split line on comma and load in class
     for line in connections_file:
         obj = line.split(',')
         connection = cs.Connections(obj[0], obj[1], obj[2])
+        connections.append(connection)
 
 def load_stations():
-    station_dict = list()
+    """Loads the stations from the CSV file into the designated object."""
     station_list = []
     station_file = open("files/StationsHolland.csv")
+    # Determine cirtical stations and add to class
     for line in station_file:
         obj = line.split(',')
         if obj[3] == "Kritiek\n":
             station = st.Stations(obj[0], obj[1], obj[2], True)
-            # Is dit wel een goed idee?
+            # Make list of critical stations
             station_list.append(station)
         else:
             station = st.Stations(obj[0], obj[1], obj[2])
+    a_solution(station_list)
+        # Adding variabels to dictionary so it can be used in quick visualisation.
+        #station_dict = list()
+        # station_dict.append({"name": obj[0]})
+        # station_dict.append({"longitude": obj[1]})
+        # station_dict.append({"latitude": obj[2]})
 
-        # Adding variabels to dictionary so it can be used in quick visualisation. 
-        station_dict.append({"name": obj[0]})
-        station_dict.append({"longitude": obj[1]})
-        station_dict.append({"latitude": obj[2]})
-        print(station_dict[0]["longitude"])
+def a_solution(station_list):
+    """Finds a set of connections"""
+    #print(random_int)
+    #print(station_list[random_int].name)
+    # zoek naam stations connections
+    # pak de eerste die je tegen komt
+    # Einde traject
+    end_time = 0
+    for station in station_list:
+        attempt = sn.Solution
+        for connection in connections:
+            if connection.begin == station.name or connection.end == station.name:
+                print(connection.begin)
+                route = [connection.begin, connection.end]
+                duration = connection.time
+                route = rt.Route(route)
+                print(route.time)
+                end_time += int(duration)
+                attempt.append(route, end_time)
+                print(end_time)
+
+                break
+
+
 
 
 

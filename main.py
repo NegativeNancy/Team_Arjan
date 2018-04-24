2658affb81136d019cf92d44c28453639817128d
Ivo den Hertog <ivodenhertog@gmail.com>
2018-04-24 14:56:32 +0200

    Small change to main.py

--- main.py
+++ main.py
@@ -3,15 +3,21 @@
 from classes import Stations as st
 from classes import Route as rt
 from classes import Solution as sn
+from algorithms import random as ra
 import loading_files as lf
 import random
 
 
+
 def main():
     connections = list()
-    holland_main()
-    exit(0)
+    station_list = holland_main()
+    random_alg()
 
 
 def holland_main():
     lf.load_stations()
+
+
+def random_alg():
+    ra.random()

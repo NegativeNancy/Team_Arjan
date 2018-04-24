936af7332ee4be421531c4235f552276f943c000
barry <barry.de.vries123@gmail.com>
2018-04-24 13:23:29 +0200

    .

--- classes/Stations.py
+++ classes/Stations.py
@@ -4,13 +4,3 @@ class Stations(object):
         self.name = name
         self.critical = critical
         self.neighbors = []
-        self.been = False
-
-    def critical_stations(self, station_list):
-        """"Loops over stations and returns a list with all critical stations."""
-
-        critical_station_list = []
-        for sation in station_list:
-            if station.critical:
-                critical_station_list.append(station)
-        return critical_station_list

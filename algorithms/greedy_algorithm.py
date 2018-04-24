5f5ef00d6d9b4d9a405f06951512c458564cadd5
emmahokken <emmahokken@gmail.com>
2018-04-24 13:30:13 +0200

    the folder exists again

---
+++ algorithms/greedy_algorithm.py
@@ -0,0 +1,19 @@
+"""Algortihm that finds the shortest path to the next critical station.
+If a critcial station cannot directly be reached, choose a random next step.
+
+Begin on a node with one critical connection.
+ga niet terug tenzij je niets anders kan
+
+maak random oplossing waarbij iedere keer een random
+"""
+
+from classes import Stations as st
+from classes import Route as rt
+from classes import Solution as sn
+import loading_files as lf
+
+def greedy():
+    station_dict = loading_files.load_stations()
+
+    # Find station with one critical unused connection.
+    for key, value in station_dict.items():

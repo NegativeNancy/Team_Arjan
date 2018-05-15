045180a6693487d588aecdd2bd107d1fc9d9c3ba
barry <barry.de.vries123@gmail.com>
2018-05-15 22:55:14 +0200

    random fix

--- algorithms/random.py
+++ algorithms/random.py
@@ -3,7 +3,7 @@ from classes import Route as rt
 from classes import Solution as sn
 import random as rd
 
-def random(station_dict, max_trains, max_minutes):
+def random(station_dict, max_trains, max_minutes, random_number_trains = True):
     """ Finds a set of random routes.
 
     Args:
@@ -17,32 +17,50 @@ def random(station_dict, max_trains, max_minutes):
 
     route_list = []
     station_dict_key_list = []
+    solution = sn.Solution(route_list, station_dict)
 
+    # Create list of keys to choose from.
     for key in station_dict:
         station_dict_key_list.append(key)
 
-    number_of_trains = 1 + rd.choice(range(int(max_trains / 2), max_trains))
+    # fix 7 trains is we want to
+    if random_number_trains:
+        number_of_trains = 1 + rd.choice(range(int(max_trains / 2), max_trains))
+    else:
+        number_of_trains = 7
+
     for i in range(number_of_trains):
-        connection_list = []
-        route = rt.Route(connection_list)
-        current_station = rd.choice(station_dict_key_list)
-        time_of_route = 0
-
-        while True:
-            next_station = rd.choice(station_dict[current_station].neighbors)
-
-            # Check whether route won't be longer than allowed.
-            if next_station[1] + route.time() < max_minutes:
-                route.append_route(current_station, next_station[0], next_station[1])/
-                current_station = next_station[0]
-                
-            # Route is too long.
-            else:
-                break
-
         # Add route to our list of routes.
+        route = create_random_route(station_dict_key_list, solution, max_minutes)
+        solution.route_list.append(route)
 
-        route_list.append(route)
-
-    solution = sn.Solution(route_list, station_dict)
     return solution, station_dict
+
+
+def create_random_route(station_dict_key_list, solution, max_minutes):
+    """Create a random route.
+    
+    Args:
+        station_dict_key_list: list of keys in station_dict.
+        max_minutes: the maximum time a train can travel.
+
+    Returns:
+        A randomly generated route.
+    """
+    connection_list = []
+    route = rt.Route(connection_list)
+    current_station = rd.choice(station_dict_key_list)
+    time_of_route = 0
+
+    while True:
+        next_station = rd.choice(solution.station_dict[current_station].neighbors)
+
+        # Check whether route won't be longer than allowed.
+        if next_station[1] + route.time() < max_minutes:
+            route.append_route(current_station, next_station[0], next_station[1])
+            current_station = next_station[0]
+
+        # Route is too long.
+        else:
+            break
+    return route
